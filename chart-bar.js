07c954480c24a2a9e67b67b512879ed899be3a93
EiichiMiyagawa <m.monobyte@gmail.com>
2022-12-06 01:33:40 +0900

    plotly.jsを使用したグラフ描画に変更

--- chart-bar.js
+++ chart-bar.js
@@ -1,88 +1,59 @@
-import * as d3 from "https://cdn.skypack.dev/d3@7";
+import "https://cdn.plot.ly/plotly-2.15.1.min.js";
 import { CSV } from "https://js.sabae.cc/CSV.js";
 
 class ChartBar extends HTMLElement {
   constructor(data) {
     super();
+    
     if (data !== undefined) {
       if (!Array.isArray(data)) {
         data = Object.keys(data).map(name => {
-          return { name, value: data[name] }
+          return { name, count: data[name] }
         });
       }
-      this.data = data;
-      console.log(this.data);
-      //this.setAttribute("value", data);
+      this.setData(data);
     } else {
+      const src = this.getAttribute("src");
+      if (src) {
+        this.fetchCsv(src);
+        return;
+      }
       const txt = this.textContent.trim();
-      const data = CSV.toJSON(CSV.decode(txt));
+      const json = CSV.toJSON(CSV.decode(txt));
       this.textContent = "";
-      if (data.length > 0) {
-        this.data = data;
-      }
+      this.setData(json);
     }
-    this.style.display = this.style.display || "inline-block";
-    this.init();
   }
-  async init() {
-    if (!this.data) {
-      const fn = this.getAttribute("src");
-      if (fn) {
-        this.data = CSV.toJSON(await CSV.fetch(fn));
-      }
-    }
-    window.addEventListener("resize", () => this.draw());
-    this.draw();
+  
+  async fetchCsv(src) {
+    const json = CSV.toJSON(await CSV.fetch(src));
+    this.setData(json);
   }
-  draw() {
-    if (this.svg) {
-      this.svg.remove();
-    }
-    const svg = this.svg = d3.select(this).append("svg");
-
-    const dataset = this.data;
-    const width = this.offsetWidth || 400;
-    const height = this.offsetHeight || 400;
-    const padding = 50; // margin for scale
+  
+  setData(data) {
+    const labels = data.map((d) => {
+      return d["name"];
+    });
+    const values = data.map((d) => {
+      return d["count"];
+    });
     
-    if (!dataset) {
-      return;
-    }
-
-    svg.attr("width", width).attr("height", height);
+    const barDatas = [{
+      type: "bar",
+      x: labels,
+      y: values
+    }];
     
-    //const color = d3.scaleSequential((t) => d3.hsl(t / 12 * 360, 1, .6));
-   
-    const xScale = d3.scaleBand()
-      .rangeRound([padding, width - padding])
-      .padding(0.1)
-      .domain(dataset.map(d => d.name));
-   
-    const yScale = d3.scaleLinear()
-      .domain([0, d3.max(dataset, d => parseFloat(d.value || d.count))])
-      .range([height - padding, padding]);
-   
-    svg.append("g")
-      .attr("transform", `translate(0, ${height - padding})`)
-      .call(d3.axisBottom(xScale));
-   
-    svg.append("g")
-      .attr("transform", `translate(${padding},0)`)
-      .call(d3.axisLeft(yScale));
-   
-    svg.append("g")
-      .selectAll("rect")
-      .data(dataset)
-      .enter()
-      .append("rect")
-      .attr("x", d => xScale(d.name))
-      .attr("y", d => yScale(d.value || d.count))
-      .attr("width", xScale.bandwidth())
-      .attr("height", d => {
-        console.log(d, yScale(d.value || d.count))
-        return height - padding - yScale(d.value || d.count)
-      })
-      .attr("fill", "steelblue");
+    const layout = {
+      xaxis: {
+        // 日付型の場合のみこのフォーマットが適用される
+        tickformat: "%Y/%m/%d"
+      },
+      yaxis: {
+        exponentformat: "none"
+      }
+    };
+    Plotly.newPlot(this, barDatas, layout);
   }
 }
 
